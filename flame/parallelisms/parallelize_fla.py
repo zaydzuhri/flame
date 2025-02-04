Fix bugs, add gradient checkpoint, add tp and others (#10)
* 1. bring activation checkpoint backs, but be aware that AC might not work for some FLA module because of some werid reason.
2. add  for easier  access.
3. some bugs fixed.
3.1 train_state.token should divide by parallel_dims.non_data_parallel_size
3.2 force creating  before
3.3 adapt  in  incase it does not exist in . [though we need to rewirte this function, mfu in future]

* 1. Fix some bugs
2. Add  to dispatch TP plans, move TP related code from  to
2.1 Add  TP plan as an example
2.2 Add  TP plan as an example, This can work only if we fixed the described changes up-stream FLA libs.

* Minor fix

* More general reshard scripts

* Fix format issues

---------

Co-authored-by: rakkit <wang55@jwlogin21.juwels>
Co-authored-by: Yu Zhang <yzhang.cs@outlook.com>

flame/parallelisms/parallelize_fla.py
@@ -7,6 +7,8 @@
 # This file applies the PT-D parallelisms (except pipeline parallelism) and various
 # training techniques (e.g. activation checkpointing and compile) to the Llama model.
 
+from collections import defaultdict
+
 import torch
 import torch.nn as nn
 from torch.distributed import DeviceMesh
@@ -14,12 +16,9 @@ from torch.distributed._composable.fsdp import (CPUOffloadPolicy,
                                                 MixedPrecisionPolicy,
                                                 fully_shard)
 from torch.distributed._composable.replicate import replicate
-from torch.distributed._tensor import Replicate, Shard
-from torch.distributed.tensor.parallel import (ColwiseParallel,
-                                               PrepareModuleInput,
-                                               RowwiseParallel,
-                                               SequenceParallel,
-                                               parallelize_module)
+from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import \
+    checkpoint_wrapper as ptd_checkpoint_wrapper
+from torch.distributed.tensor.parallel import parallelize_module
 
 from torchtitan.config_manager import TORCH_DTYPE_MAP, JobConfig
 from torchtitan.logging import logger
@@ -53,6 +52,10 @@ def parallelize_fla(
             enable_float8=job_config.float8.enable_float8_linear,
             enable_async_tp=job_config.experimental.enable_async_tensor_parallel,
         )
+
+    if job_config.activation_checkpoint.mode != "none":
+        # #! TODO[flame]: for some reasons, FLA's model might not work with activation checkpointing
+        apply_ac(model, job_config.activation_checkpoint)
 
     # turn on per-TransformerBlock compile after AC wrapping and before FSDP
     if job_config.training.compile:
@@ -113,74 +116,24 @@ def apply_tp(
     # transformer block's inputs)
     # 2. Parallelize the root norm layer over the sequence dim
     # 3. Parallelize the final linear output layer
-    parallelize_module(
-        model,
-        tp_mesh,
-        {
-            "model.model.embeddings": RowwiseParallel(
-                input_layouts=Replicate(),
-                output_layouts=Shard(1),
-            ),
-            "model.model.norm": SequenceParallel(),
-            "model.lm_head": ColwiseParallel(
-                input_layouts=Shard(1),
-                output_layouts=Shard(-1) if loss_parallel else Replicate(),
-                use_local_output=not loss_parallel,
-            ),
-        },
-    )
+    from flame.parallelisms.tp_helper import dispatch_tp_plan
 
-    # Parallel styles used for transformer block linear weights and their
-    # inputs may be different for float8 linears
-    if enable_float8:
-        # TODO(vkuzo): once float8 configuration supports delayed scaling,
-        # add a check here to enforce supported float8 all-gather configurations
-        # TODO(vkuzo): add the items below to __init__.py of torchao.float8 and import from there
-        from torchao.float8.float8_tensor_parallel import (
-            Float8ColwiseParallel, Float8RowwiseParallel,
-            PrepareFloat8ModuleInput)
+    tp_plan = dispatch_tp_plan(model, loss_parallel, enable_float8)
 
-        rowwise_parallel, colwise_parallel, prepare_module_input = (
-            Float8RowwiseParallel,
-            Float8ColwiseParallel,
-            PrepareFloat8ModuleInput,
-        )
+    parallelize_module(model, tp_mesh, tp_plan.others_plan)
+
+    actual_transformer_blocks = monkey_patch_get_real_transformer_block(model)
+    if actual_transformer_blocks is None:
+        logger.warning("No TransformerBlock found for tensor parallelism")
     else:
-        rowwise_parallel, colwise_parallel, prepare_module_input = (
-            RowwiseParallel,
-            ColwiseParallel,
-            PrepareModuleInput,
-        )
+        for layer_id, transformer_block in enumerate(actual_transformer_blocks):
+            layer_plan = dispatch_tp_plan(transformer_block).layer_plan
 
-    # Apply tensor + sequence parallelism to every transformer block
-    # NOTE: At the cost of model code change, we can accelerate Sequence Parallel
-    #       by folding (and unfolding) the batch dimension and the sequence dimension.
-    #       Examples can be found at https://github.com/pytorch/torchtitan/pull/437
-    for _, block in enumerate(getattr(model, model.base_model_prefix).layers):
-        layer_plan = {
-            "attn_norm": SequenceParallel(),
-            "attn": prepare_module_input(
-                input_layouts=(Shard(1), None),
-                desired_input_layouts=(Replicate(), None),
-            ),
-            "attn.q_proj": colwise_parallel(),
-            "attn.k_proj": colwise_parallel(),
-            "attn.v_proj": colwise_parallel(),
-            "attn.o_proj": rowwise_parallel(output_layouts=Shard(1)),
-            "mlp_norm": SequenceParallel(),
-            "mlp": prepare_module_input(
-                input_layouts=(Shard(1),),
-                desired_input_layouts=(Replicate(),),
-            ),
-            "mlp.gate_proj": colwise_parallel(),
-            "mlp.down_proj": rowwise_parallel(output_layouts=Shard(1))
-        }
-
-        parallelize_module(
-            module=block,
-            device_mesh=tp_mesh,
-            parallelize_plan=layer_plan,
-        )
+            parallelize_module(
+                module=transformer_block,
+                device_mesh=tp_mesh,
+                parallelize_plan=layer_plan,
+            )
 
     if enable_async_tp:
         from torch.distributed._symmetric_memory import \
@@ -208,16 +161,95 @@ _save_list = {
 }
 
 
+def _apply_ac_to_transformer_block(module: nn.Module, ac_config):
+    valid_ac_modes = ("full", "selective")
+    if ac_config.mode not in valid_ac_modes:
+        raise ValueError(
+            f"Invalid AC mode: {ac_config.mode}. Valid modes: {valid_ac_modes}"
+        )
+
+    if ac_config.mode == "full":
+        return ptd_checkpoint_wrapper(module, preserve_rng_state=False)
+
+    assert ac_config.mode == "selective", f"{ac_config.mode}"
+    use_op_sac = ac_config.selective_ac_option == "op"
+    use_layer_sac = ac_config.selective_ac_option.isdigit()
+    if not use_op_sac and not use_layer_sac:
+        raise ValueError(
+            f"Invalid selective AC option: {ac_config.selective_ac_option}. "
+            f"Valid options: 'op' or a positive int representing layer frequency"
+        )
+    if use_op_sac:
+        from torch.utils.checkpoint import (
+            CheckpointPolicy, create_selective_checkpoint_contexts)
+
+        def _get_custom_policy(meta):
+            def _custom_policy(ctx, func, *args, **kwargs):
+                mode = "recompute" if ctx.is_recompute else "forward"
+                mm_count_key = f"{mode}_mm_count"
+                if func == torch.ops.aten.mm.default:
+                    meta[mm_count_key] += 1
+                # Saves output of all compute ops, except every second mm
+                to_save = func in _save_list and not (
+                    func == torch.ops.aten.mm.default and meta[mm_count_key] % 2 == 0
+                )
+                return (
+                    CheckpointPolicy.MUST_SAVE
+                    if to_save
+                    else CheckpointPolicy.PREFER_RECOMPUTE
+                )
+
+            return _custom_policy
+
+        def selective_checkpointing_context_fn():
+            meta = defaultdict(int)
+            return create_selective_checkpoint_contexts(_get_custom_policy(meta))
+
+        return ptd_checkpoint_wrapper(
+            module,
+            context_fn=selective_checkpointing_context_fn,
+            preserve_rng_state=False,
+        )
+    elif use_layer_sac:
+        # Checkpoint every `ac_freq` of the modules passed to this function
+        ac_freq = int(ac_config.selective_ac_option)
+        ptd_checkpoint_wrapper.__dict__.setdefault("_count", 0)
+        ptd_checkpoint_wrapper._count += 1
+        if not ac_freq or ptd_checkpoint_wrapper._count % ac_freq == 0:
+            return ptd_checkpoint_wrapper(module, preserve_rng_state=False)
+        else:
+            return module
+
+
+def apply_ac(model: nn.Module, ac_config):
+    """Apply activation checkpointing to the model."""
+
+    actual_transformer_blocks = monkey_patch_get_real_transformer_block(model)
+    if actual_transformer_blocks is None:
+        logger.warning("No TransformerBlock found for activation checkpointing")
+        return
+
+    for layer_id, transformer_block in actual_transformer_blocks.named_children():
+        transformer_block = _apply_ac_to_transformer_block(transformer_block, ac_config)
+        actual_transformer_blocks.register_module(layer_id, transformer_block)
+
+    logger.info(f"Applied {ac_config.mode} activation checkpointing to the model")
+
+
 def apply_compile(model: nn.Module):
     """
     Apply torch.compile to each TransformerBlock, which makes compilation efficient due to
     repeated structure. Alternatively one can compile the whole model (after applying DP).
     """
-    for layer_id, block in getattr(model, model.base_model_prefix).layers.named_children():
-        block = torch.compile(block, fullgraph=True)
-        getattr(model, model.base_model_prefix).layers.register_module(layer_id, block)
 
-    logger.info("Compiling each TransformerBlock with torch.compile")
+    actual_transformer_blocks = monkey_patch_get_real_transformer_block(model)
+    if actual_transformer_blocks is None:
+        logger.warning("No TransformerBlock found for torch.compile")
+    else:
+        for layer_id, block in actual_transformer_blocks.named_children():
+            block = torch.compile(block, fullgraph=True)
+            actual_transformer_blocks.register_module(layer_id, block)
+        logger.info("Compiling each TransformerBlock with torch.compile")
 
 
 def apply_fsdp(
@@ -236,20 +268,27 @@ def apply_fsdp(
     if cpu_offload:
         fsdp_config["offload_policy"] = CPUOffloadPolicy()
 
-    for layer_id, block in enumerate(getattr(model, model.base_model_prefix).layers):
-        if pp_enabled:
-            # For PP, do not reshard after forward to avoid per-microbatch
-            # all-gathers, which can be expensive and non-overlapped
-            reshard_after_forward = False
-        else:
-            # As an optimization, do not reshard after forward for the last
-            # transformer block since FSDP would prefetch it immediately
-            reshard_after_forward = int(layer_id) < len(getattr(model, model.base_model_prefix).layers) - 1
-        fully_shard(
-            block,
-            **fsdp_config,
-            reshard_after_forward=reshard_after_forward,
-        )
+    actual_transformer_blocks = monkey_patch_get_real_transformer_block(model)
+    if actual_transformer_blocks is None:
+        logger.warning("No TransformerBlock found for FSDP")
+
+    else:
+        total_blocks = len(actual_transformer_blocks)
+        for layer_id, block in enumerate(actual_transformer_blocks):
+            if pp_enabled:
+                # For PP, do not reshard after forward to avoid per-microbatch
+                # all-gathers, which can be expensive and non-overlapped
+                reshard_after_forward = False
+            else:
+                # As an optimization, do not reshard after forward for the last
+                # transformer block since FSDP would prefetch it immediately
+                reshard_after_forward = int(layer_id) < total_blocks - 1
+            fully_shard(
+                block,
+                **fsdp_config,
+                reshard_after_forward=reshard_after_forward,
+            )
+
     fully_shard(model, **fsdp_config, reshard_after_forward=not pp_enabled)
 
 
@@ -270,3 +309,18 @@ def apply_ddp(
     replicate(model, device_mesh=dp_mesh, bucket_cap_mb=100)
 
     logger.info("Applied DDP to the model")
+
+
+# ================================================================================================
+# below are monkey-patched functions for the FLA model
+
+
+def monkey_patch_get_real_transformer_block(model):
+    # TODO[flame]: adapt for network not using 'layers' attribute
+    base_model_prefix = getattr(model, "base_model_prefix", "model")
+    if not hasattr(model, base_model_prefix):
+        return None
+    if not hasattr(getattr(model, base_model_prefix), "layers"):
+        logger.warning('no "layers" in model for activation checkpointing')
+        return None
+    return getattr(model, base_model_prefix).layers

flame/parallelisms/pipeline_fla.py
@@ -13,13 +13,14 @@ import torch
 import torch.nn as nn
 from torch.distributed import DeviceMesh
 from torch.distributed.pipelining import PipelineStage
+from transformers import PretrainedConfig
+
 from torchtitan.config_manager import JobConfig
 from torchtitan.logging import logger
 from torchtitan.parallelisms.parallel_dims import ParallelDims
 from torchtitan.parallelisms.pipelining_utils import (build_pipeline_schedule,
                                                       generate_split_points,
                                                       stage_ids_this_rank)
-from transformers import PretrainedConfig
 
 DeviceType = Union[int, str, torch.device]
 

flame/utils.py
@@ -270,15 +270,25 @@ def init_distributed(job_config):
 def get_num_params(model: torch.nn.Module, exclude_embedding: bool = False) -> int:
     num_params = sum(p.numel() for p in model.parameters())
     if exclude_embedding:
-        num_params -= sum(i.num_parameters() for i in model.children() if isinstance(i, nn.Embedding))
+        num_params -= sum(
+            i.num_parameters() for i in model.children() if isinstance(i, nn.Embedding)
+        )
     return num_params
 
 
 def get_num_flop_per_token(num_params: int, model_config, seq_len) -> int:
+    if hasattr(model_config, "num_heads"):
+        num_heads = model_config.num_heads
+    elif hasattr(model_config, "num_attention_heads"):
+        num_heads = model_config.num_attention_heads
+    else:
+        num_heads = 1
+        logger.warning("num_heads not found in model_config, defaulting to 1. ")
+
     l, h, q, t = (
         model_config.num_hidden_layers,
-        model_config.num_heads,
-        model_config.hidden_size // model_config.num_heads,
+        num_heads,
+        model_config.hidden_size // num_heads,
         seq_len,
     )
     # Reasoning behind the factor of 12 for the self-attention part of the formula:
@@ -397,9 +407,10 @@ def get_total_norm(
 
     norms: List[torch.Tensor] = []
     for (device, _), ([device_tensors], _) in grouped_tensors.items():
-        if (foreach is None and nn.utils.clip_grad._has_foreach_support(device_tensors, device)) or (
-            foreach and nn.utils.clip_grad._device_has_foreach_support(device)
-        ):
+        if (
+            foreach is None
+            and nn.utils.clip_grad._has_foreach_support(device_tensors, device)
+        ) or (foreach and nn.utils.clip_grad._device_has_foreach_support(device)):
             norms.extend(torch._foreach_norm(device_tensors, norm_type))
         elif foreach:
             raise RuntimeError(
@@ -474,9 +485,10 @@ def clip_grads_with_norm_(
     # when the gradients do not reside in CPU memory.
     clip_coef_clamped = torch.clamp(clip_coef, max=1.0)
     for (device, _), ([device_grads], _) in grouped_grads.items():
-        if (foreach is None and nn.utils.clip_grad._has_foreach_support(device_grads, device)) or (
-            foreach and nn.utils.clip_grad._device_has_foreach_support(device)
-        ):
+        if (
+            foreach is None
+            and nn.utils.clip_grad._has_foreach_support(device_grads, device)
+        ) or (foreach and nn.utils.clip_grad._device_has_foreach_support(device)):
             torch._foreach_mul_(device_grads, clip_coef_clamped.to(device))
         elif foreach:
             raise RuntimeError(
@@ -524,9 +536,7 @@ def clip_grad_norm_(
 
     """
     grads = [p.grad for p in parameters if p.grad is not None]
-    total_norm = get_total_norm(
-        grads, norm_type, error_if_nonfinite, foreach
-    )
+    total_norm = get_total_norm(grads, norm_type, error_if_nonfinite, foreach)
 
     # If total_norm is a DTensor, the placements must be `torch.distributed._tensor.ops.math_ops._NormPartial`.
     # We can simply reduce the DTensor to get the total norm in this tensor's process group

train.py
@@ -93,8 +93,10 @@ def main(job_config: JobConfig):
         model_max_length=int(1e10)
     )
     logger.info(f"{tokenizer}")
-    logger.info(f"Loading dataset {job_config.training.dataset}"
-                f":{job_config.training.dataset_name}" if job_config.training.dataset_name is not None else "")
+    logger.info(
+        f"Loading dataset {job_config.training.dataset}"
+        f":{job_config.training.dataset_name}" if job_config.training.dataset_name is not None else ""
+    )
 
     min_num_shards = dp_degree * job_config.training.num_workers
     if len(job_config.training.dataset.split(',')) == 1:
@@ -113,7 +115,9 @@ def main(job_config: JobConfig):
         logger.info(f"Shuffling the dataset with seed {job_config.training.seed}")
         if not job_config.training.streaming:
             # the states of map-style dataset is recoverable after shuffling
-            dataset = dataset.shuffle(seed=job_config.training.seed).to_iterable_dataset(num_shards=min_num_shards)
+            dataset = dataset.shuffle(
+                seed=job_config.training.seed
+            ).to_iterable_dataset(num_shards=min_num_shards)
         else:
             if dataset.num_shards < min_num_shards:
                 logger.warning(
@@ -124,16 +128,20 @@ def main(job_config: JobConfig):
                     f"Resharding dataset to {min_num_shards} shards and disabling streaming mode."
                     f"{color.reset}"
                 )
-                dataset = load_dataset(
-                    path=job_config.training.dataset,
-                    name=getattr(job_config.training, 'dataset_name', None),
-                    data_dir=getattr(job_config.training, 'data_dir', None),
-                    data_files=getattr(job_config.training, 'data_files', None),
-                    split=job_config.training.dataset_split or "train",
-                    trust_remote_code=True,
-                    streaming=False,
-                    num_proc=job_config.training.num_workers
-                ).shuffle(seed=job_config.training.seed).to_iterable_dataset(num_shards=min_num_shards)
+                dataset = (
+                    load_dataset(
+                        path=job_config.training.dataset,
+                        name=getattr(job_config.training, "dataset_name", None),
+                        data_dir=getattr(job_config.training, "data_dir", None),
+                        data_files=getattr(job_config.training, "data_files", None),
+                        split=job_config.training.dataset_split or "train",
+                        trust_remote_code=True,
+                        streaming=False,
+                        num_proc=job_config.training.num_workers,
+                    )
+                    .shuffle(seed=job_config.training.seed)
+                    .to_iterable_dataset(num_shards=min_num_shards)
+                )
             else:
                 dataset = shuffle(dataset, seed=job_config.training.seed)
     else:
@@ -198,19 +206,27 @@ def main(job_config: JobConfig):
                     )
                     # again, it's ok to directly shuffle the map-style dataset
                     # we expect an error raised if the map-style dataset still has not enough data shards
-                    subset = load_dataset(
-                        path=datasets[i],
-                        name=dataset_names[i],
-                        data_dir=data_dirs[i],
-                        data_files=data_files[i],
-                        split=dataset_splits[i],
-                        trust_remote_code=True,
-                        streaming=False,
-                        num_proc=job_config.training.num_workers
-                    ).shuffle(seed=job_config.training.seed).to_iterable_dataset(min_num_shards)
+                    subset = (
+                        load_dataset(
+                            path=datasets[i],
+                            name=dataset_names[i],
+                            data_dir=data_dirs[i],
+                            data_files=data_files[i],
+                            split=dataset_splits[i],
+                            trust_remote_code=True,
+                            streaming=False,
+                            num_proc=job_config.training.num_workers,
+                        )
+                        .shuffle(seed=job_config.training.seed)
+                        .to_iterable_dataset(min_num_shards)
+                    )
                 else:
                     # we set relatively small buffer size here as interleaving could provide some randomness
-                    subset = shuffle(subset, seed=job_config.training.seed, buffer_size=max(128, 1024 // len(datasets)))
+                    subset = shuffle(
+                        subset,
+                        seed=job_config.training.seed,
+                        buffer_size=max(128, 1024 // len(datasets)),
+                    )
 
             if 'text' in subset.column_names:
                 subset = subset.select_columns('text')
@@ -410,8 +426,29 @@ def main(job_config: JobConfig):
                 data_loading_times.append(time.perf_counter() - data_load_start)
 
                 input_ids = input_ids.to(device_type)
+
+                """
+                TODO[flame]: We need to carefully handle the position_ids for TP/CP
+                Depending on the Models'PE, the position_ids might be different.
+
+                e.g. for TP
+                    For RoPE, all ranks have the same position_ids. [FOR HF model]
+                    For sinusoidal, each rank has the coresponding chunked  position_ids. [FOR HF model]
+
+                e.g. for CP, [optional_context_parallel_ctx shoudl automatically distbute the position_ids]
+                    Each rank has the coresponding chunked position_ids. [FOR All model]
+
+                """
+                position_ids = torch.arange(
+                    0, input_ids.shape[1], device=device_type
+                ).repeat(input_ids.shape[0], 1)
+
                 labels = labels.to(device_type)
-                cu_seqlens = batch['cu_seqlens'].to(device_type) if 'cu_seqlens' in batch else None
+                cu_seqlens = (
+                    batch["cu_seqlens"].to(device_type)
+                    if "cu_seqlens" in batch
+                    else None
+                )
                 # apply context parallelism if cp is enabled
                 optional_context_parallel_ctx = (
                     utils.create_context_parallel_ctx(
@@ -424,6 +461,7 @@ def main(job_config: JobConfig):
                     if parallel_dims.cp_enabled
                     else None
                 )
+                # #! TODO[flame], we should distribute the position_ids as well with CP
 
                 if parallel_dims.pp_enabled:
                     # Pipeline Parallel forward / backward inside step() call
@@ -448,7 +486,12 @@ def main(job_config: JobConfig):
                 else:
                     # Non-PP forward / backward
                     with train_context(optional_context_parallel_ctx):
-                        output = model(input_ids=input_ids, labels=labels, cu_seqlens=cu_seqlens)
+                        output = model(
+                            input_ids=input_ids,
+                            labels=labels,
+                            position_ids=position_ids,
+                            cu_seqlens=cu_seqlens,
+                        )
                         loss = output.loss
                         loss.backward()
                 losses.append(loss)
@@ -480,7 +523,10 @@ def main(job_config: JobConfig):
             float8_handler.precompute_float8_dynamic_scale_for_fsdp(model_parts)
 
             # log metrics
-            if train_state.step == 1 or train_state.step % job_config.metrics.log_freq == 0:
+            if (
+                train_state.step == 1
+                or train_state.step % job_config.metrics.log_freq == 0
+            ):
                 if (
                     parallel_dims.dp_replicate_enabled
                     or parallel_dims.dp_shard_enabled
@@ -496,10 +542,27 @@ def main(job_config: JobConfig):
 
                 time_delta = time.perf_counter() - time_last_log
 
+                """
+                TODO[flame]: check this after fixing TP/PP/CP, if we assume all ranks have the same number of tokens
+                we can just multiple it by DP's dims, this avoiding to deal with the case taht dp does nto exists
+                """
                 # update train state
-                train_state.token += utils.dist_reduce(
-                    torch.tensor(ntokens_since_last_log, device=device), "sum", world_mesh["dp_cp"]
+                # train_state.token += (
+                #     utils.dist_reduce(
+                #         torch.tensor(ntokens_since_last_log, device=device),
+                #         "sum",
+                #         world_mesh["dp_cp"],
+                #     )
+                #     / parallel_dims.non_data_parallel_size
+                # )
+
+                # update train state
+                train_state.token += (
+                    ntokens_since_last_log
+                    * parallel_dims.world_size
+                    / parallel_dims.non_data_parallel_size
                 )
+
                 train_state.elapsed += timedelta(seconds=time_delta)
                 train_state.log_steps.append(train_state.step)
                 train_state.global_avg_losses.append(global_avg_loss)