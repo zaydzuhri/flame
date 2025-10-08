a0dcfddd30dd349678441ab038f0ec9e96ea7864
zaydzuhri <zaydzuhri@gmail.com>
2025-10-08 11:05:08 +0400

    Fix setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -36,9 +36,9 @@ setup(
     ],
     python_requires='>=3.10',
     install_requires=[
-        'torch>=2.5',
+        'torch==2.6',
         'torchdata',
-        'transformers>=4.45.0',
+        'transformers==4.51.3',
         'triton>=3.0',
         'datasets>=3.3.0',
         'einops',
@@ -46,5 +46,6 @@ setup(
         'wandb',
         'tiktoken',
         'tensorboard',
+        'python-dotenv'
     ],
 )
