76d1734c084b06431cec124c71eafa50f896add9
zaydzuhri <zaydzuhri@gmail.com>
2026-08-10 12:12:03 +0000

    Add layernorm to GRU and LSTM

--- custom_models/gru/gru_kernels/naive.py
+++ custom_models/gru/gru_kernels/naive.py
@@ -15,7 +15,23 @@ def gru_step_torch(x_t: torch.Tensor, h_t: torch.Tensor, h_prev: torch.Tensor) -
     xr, xz, xn = x_t.chunk(3, dim=-1)
     hr, hz, hn = h_t.chunk(3, dim=-1)
 
-    r_t = torch.sigmoid(xr + hr)
-    z_t = torch.sigmoid(xz + hz)
-    n_t = torch.tanh(xn + r_t * hn)
+    # r_pre = torch.nn.functional.layer_norm(xr + hr, normalized_shape=(xr + hr).shape[-1:], eps=1e-5)
+    # z_pre = torch.nn.functional.layer_norm(xz + hz, normalized_shape=(xz + hz).shape[-1:], eps=1e-5)
+    # n_pre = torch.nn.functional.layer_norm(xn + hn, normalized_shape=(xn + hn).shape[-1:], eps=1e-5)
+
+    # r_t = torch.sigmoid(r_pre)
+    # z_t = torch.sigmoid(z_pre)
+    # n_t = torch.tanh(n_pre + r_t * hn)
+
+    # do per component layernorm
+    xr_pre = torch.nn.functional.layer_norm(xr, normalized_shape=(xr.shape[-1],), eps=1e-5)
+    hr_pre = torch.nn.functional.layer_norm(hr, normalized_shape=(hr.shape[-1],), eps=1e-5)
+    xz_pre = torch.nn.functional.layer_norm(xz, normalized_shape=(xz.shape[-1],), eps=1e-5)
+    hz_pre = torch.nn.functional.layer_norm(hz, normalized_shape=(hz.shape[-1],), eps=1e-5)
+    xn_pre = torch.nn.functional.layer_norm(xn, normalized_shape=(xn.shape[-1],), eps=1e-5)
+    hn_pre = torch.nn.functional.layer_norm(hn, normalized_shape=(hn.shape[-1],), eps=1e-5)
+
+    r_t = torch.sigmoid(xr_pre + hr_pre)
+    z_t = torch.sigmoid(xz_pre + hz_pre)
+    n_t = torch.tanh(xn_pre + r_t * hn_pre)
     return (1.0 - z_t) * n_t + z_t * h_prev
--- custom_models/lstm/lstm_kernels/naive.py
+++ custom_models/lstm/lstm_kernels/naive.py
@@ -23,11 +23,30 @@ def lstm_step_torch(
     xi, xf, xg, xo = x_t.chunk(4, dim=-1)
     hi, hf, hg, ho = h_t.chunk(4, dim=-1)
 
-    i_t = torch.sigmoid(xi + hi)
-    f_t = torch.sigmoid(xf + hf)
-    g_t = torch.tanh(xg + hg)
-    o_t = torch.sigmoid(xo + ho)
+    # i_t = torch.sigmoid(xi + hi)
+    # f_t = torch.sigmoid(xf + hf)
+    # g_t = torch.tanh(xg + hg)
+    # o_t = torch.sigmoid(xo + ho)
+
+    # c_next = f_t * c_prev + i_t * g_t
+    # h_next = o_t * torch.tanh(c_next)
+
+    # do per component layernorm
+    xi_pre = torch.nn.functional.layer_norm(xi, normalized_shape=(xi.shape[-1],), eps=1e-5)
+    hi_pre = torch.nn.functional.layer_norm(hi, normalized_shape=(hi.shape[-1],), eps=1e-5)
+    xf_pre = torch.nn.functional.layer_norm(xf, normalized_shape=(xf.shape[-1],), eps=1e-5)
+    hf_pre = torch.nn.functional.layer_norm(hf, normalized_shape=(hf.shape[-1],), eps=1e-5)
+    xg_pre = torch.nn.functional.layer_norm(xg, normalized_shape=(xg.shape[-1],), eps=1e-5)
+    hg_pre = torch.nn.functional.layer_norm(hg, normalized_shape=(hg.shape[-1],), eps=1e-5)
+    xo_pre = torch.nn.functional.layer_norm(xo, normalized_shape=(xo.shape[-1],), eps=1e-5)
+    ho_pre = torch.nn.functional.layer_norm(ho, normalized_shape=(ho.shape[-1],), eps=1e-5)
+
+    i_t = torch.sigmoid(xi_pre + hi_pre)
+    f_t = torch.sigmoid(xf_pre + hf_pre)
+    g_t = torch.tanh(xg_pre + hg_pre)
+    o_t = torch.sigmoid(xo_pre + ho_pre)
 
     c_next = f_t * c_prev + i_t * g_t
     h_next = o_t * torch.tanh(c_next)
+
     return h_next, c_next
